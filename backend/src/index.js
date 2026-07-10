5ab7c2beaa5330c7798c11797e9a5319a00a359c
Smartdvis17 <ansal_310@outlook.com>
2026-07-10 15:47:48 -0500

    Cargue frontend inicial

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(morgan('dev'));
 app.use(cors({
-    origin: 'http://localhost:3000'}));
+    origin: 'http://localhost:5174'}));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use("/tasks", routes);
